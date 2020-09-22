a748d81c206d6cd5b03b2f2904e5187ab1293782
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-22 01:33:16 -0400

    image sized and centered on about page

--- reactportfolio1/src/pages/About/About.js
+++ reactportfolio1/src/pages/About/About.js
@@ -1,14 +1,17 @@
 import React from 'react'
-
+import './About.css'
+import Image1 from '../../components/Assets/Images/Dave.jpeg'
 import {
   Card, Button, CardHeader, CardFooter, CardBody,
   CardTitle, CardText
 } from 'reactstrap';
+
 const Portfolio = () => {
   return (
     <>
-      <h1>hi</h1>
-
+    
+        <img className="size" src={Image1} alt=""/>
+     
     </>
   )
 }
