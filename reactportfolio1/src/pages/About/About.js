a7e9c6755be70ee989a235b45d5d92727cb376e0
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-22 01:54:20 -0400

    name,email,about links on about page

--- reactportfolio1/src/pages/About/About.js
+++ reactportfolio1/src/pages/About/About.js
@@ -9,8 +9,30 @@ import {
 const Portfolio = () => {
   return (
     <>
-    
+      <h1 className="name" >Dave Trimboli</h1>
+      <hr/>
+      <h5 className="name">datrimboli@gmail.com</h5>
+      <h5 className="name">(914) 489-1516</h5>
+      <hr/>
         <img className="size" src={Image1} alt=""/>
+        <hr/>
+      <div className="name">
+        <Button className="buttoncolor bottom" type="button"
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = 'https://github.com/datrimboli';
+        }}
+      > Visit my Github!</Button>
+      
+      <Button className="buttoncolor bottom" type="button"
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = 'https://www.linkedin.com/in/dave-trimboli-158a16a4/';
+        }}
+      > Visit my LinkedIn!</Button>
+      
+      </div>
+        
      
     </>
   )
