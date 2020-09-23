f1493f58a44fe806c8647de0deff264b56a1588b
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-23 12:52:38 -0400

    Updates

---
+++ precache-manifest.a14365238f61f37b9c955adf3f68786a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286f8a8589ba73046d217bf7b9f5aed1",
+    "url": "/reactportfolio1/index.html"
+  },
+  {
+    "revision": "b81fa18ad0c859664f3e",
+    "url": "/reactportfolio1/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c2d5a08d65bf6a0d8fac",
+    "url": "/reactportfolio1/static/css/main.a77b2fdc.chunk.css"
+  },
+  {
+    "revision": "b81fa18ad0c859664f3e",
+    "url": "/reactportfolio1/static/js/2.82895f7a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactportfolio1/static/js/2.82895f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d5a08d65bf6a0d8fac",
+    "url": "/reactportfolio1/static/js/main.efa84187.chunk.js"
+  },
+  {
+    "revision": "7330acf9f0e74f3351fe",
+    "url": "/reactportfolio1/static/js/runtime-main.92406004.js"
+  },
+  {
+    "revision": "b5ba9fff2ceceacd26ed047fa372e7d3",
+    "url": "/reactportfolio1/static/media/Dave.b5ba9fff.jpeg"
+  },
+  {
+    "revision": "f0a2b6a24a324a88c8517a90278dae99",
+    "url": "/reactportfolio1/static/media/bit.f0a2b6a2.png"
+  },
+  {
+    "revision": "14bc7f881c0f8b8eb70f15e6c22a39f2",
+    "url": "/reactportfolio1/static/media/burgproj.14bc7f88.png"
+  },
+  {
+    "revision": "90c29e6ba84d4ff28377d67b4bfc5d14",
+    "url": "/reactportfolio1/static/media/chart.90c29e6b.png"
+  },
+  {
+    "revision": "8337662530d6b8de58d4403280541a23",
+    "url": "/reactportfolio1/static/media/note.83376625.png"
+  },
+  {
+    "revision": "842b4b6e70048fd6ec31543427cefcce",
+    "url": "/reactportfolio1/static/media/poke.842b4b6e.png"
+  },
+  {
+    "revision": "197af821fa0ff9ccaafc41ac2e9244e7",
+    "url": "/reactportfolio1/static/media/resumecurrent.197af821.pdf"
+  },
+  {
+    "revision": "198bdb9476b87bdc7e30394bcc9f424a",
+    "url": "/reactportfolio1/static/media/workout.198bdb94.png"
+  }
+]);
